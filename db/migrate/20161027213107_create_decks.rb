a637e53176c0cfbfee59964319b99e7655082d86
Drake Stillman <drakimon@gmail.com>
2016-11-29 11:09:27 +0000

    Implement basic login

--- db/migrate/20161027213107_create_decks.rb
+++ db/migrate/20161027213107_create_decks.rb
@@ -1,9 +1,9 @@
 class CreateDecks < ActiveRecord::Migration[5.0]
   def change
     create_table :decks do |t|
-      t.integer :deckId
-      t.string :faction
-      t.string :userId
+      t.integer  :deckName
+      t.string   :faction
+      t.string   :user_id
 
       t.timestamps
     end
