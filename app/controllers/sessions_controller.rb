c96b7b71465a66a6f0a1b6da3d046480defb0633
Drake Stillman <drakimon@gmail.com>
2016-11-29 10:42:30 +0000

    Fix an unexpected schema revert

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+    else
+      flash[:danger] = 'Invalid login/password' # Incorrect
+      render 'new'
+    end
+  end
+  
+  def destroy
+  end
+end
